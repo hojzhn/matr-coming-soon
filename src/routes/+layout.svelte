e7431e9c915ea3589e3661e5e648cf4b46250fde
Savvy <aaalex.hn@gmail.com>
2026-08-13 16:59:10 -0400

    -

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,8 +5,8 @@
 </script>
 
 <svelte:head>
-	<title>matr labs: Coming Soon</title>
-	<meta name="description" content="Oil prints made to order. matr labs is coming back soon." />
+	<title>matr labs</title>
+	<meta name="description" content="Oil prints made to order. Matr labs is coming back soon." />
 </svelte:head>
 
 {@render children()}
